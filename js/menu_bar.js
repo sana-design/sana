2fe3ddcd063af666cad1fff171b1d13df24259d5
sana-design <puppig19@nate.com>
2020-06-21 23:38:07 +0900

    first commit

---
+++ js/menu_bar.js
@@ -0,0 +1,16 @@
+
+
+$(function() {
+    $('#line-wrapper').click(function(){
+        $('.line').removeClass('init');
+        $('#line-top').toggleClass('line-top').toggleClass('top-reverse');
+        $('#line-mid').toggleClass('line-mid').toggleClass('mid-reverse');
+        $('#line-bot').toggleClass('line-bot').toggleClass('bot-reverse');
+    })
+});
+
+
+
+$('#line-wrapper').on('click', function() {
+    $('.menu_bar').toggleClass('active')
+});
